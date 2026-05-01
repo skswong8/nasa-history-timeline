8ebf61357b7f676d79ec2f23ce715b22b06896b9
Sam Wong <samuelwong28@gmail.com>
2026-05-01 17:12:15 +0800

    Add logo and add images associated with data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import TimelineClient from './components/TimelineClient';
+import Image from 'next/image'
 
 /**
  * Home SSR entrypoint.
@@ -9,5 +10,20 @@ export default async function Home() {
 	const fetchEras = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/timeline/eras`);
 	const eras = await fetchEras.json();
 
-	return <TimelineClient eras={ eras } />
+	return (
+		<>
+			<header>
+				<div className="container mx-auto p-4">
+					<Image
+						src="/nasa-logo.svg"
+						alt="NASA logo"
+						width={ 80 }
+						height={ 66 }
+						priority
+					/>
+				</div>
+			</header>
+			<TimelineClient eras={ eras } />
+		</>
+	)
 }
