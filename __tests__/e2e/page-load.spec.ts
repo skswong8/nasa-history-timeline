24c715902b19725050a8a95f32ce0cf3fb748069
Sam Wong <samuelwong28@gmail.com>
2026-05-13 16:37:22 +0800

    Add more page load e2e tests

--- __tests__/e2e/page-load.spec.ts
+++ __tests__/e2e/page-load.spec.ts
@@ -8,4 +8,19 @@ test.describe('Page load', () => {
 	test('has title', async ({ page }) => {
 		await expect(page).toHaveTitle(/NASA History Timeline/)
 	})
+
+	test('has logo', async ({ page }) => {
+		const logo = page.getByAltText(/NASA logo/)
+		await expect(logo).toBeVisible()
+	})
+
+	test('has description', async ({ page }) => {
+		const description = page.getByRole('paragraph')
+		await expect(description).toBeVisible()
+	})
+
+	test('first era is selected by default', async ({ page }) => {
+		const firstEraButton = page.getByRole('button', { name: /1958/i })
+		await expect(firstEraButton).toHaveClass(/timeline-filter__item--active/)
+	})
 })
